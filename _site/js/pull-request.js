596772f023b4d32c5d119885e7ca0be554d32501
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2012-07-03 13:19:10 -0400

    unignore _site

---
+++ _site/js/pull-request.js
@@ -0,0 +1,16 @@
+////Idea taken from https://github.com/dfm
+///https://raw.github.com/dfm/dfm.github.com/master/assets/comments.js
+
+var github_url = "https://github.com/brunosan/brunosan.eu/edit/gh-pages/_posts/";
+
+$(function() {
+
+    $("#pull-request-btn").click(function () {
+        var data = $(this).data();
+        var path = data.title.split("/");
+        var url = github_url + data.date + "-" + path[path.length-1] + ".md";
+        window.location = url;
+        return false;
+    });
+});
+
